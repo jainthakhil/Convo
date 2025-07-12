08255cba9faa4aed99e1aead225a2d2780aca082
Apoorva Panwar <apoorvapanwar488@gmail.com>
2025-07-12 19:50:06 +0530

    signup page done for medium screen

--- convo-app/src/App.jsx
+++ convo-app/src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
     <div className='app h-screen flex overflow-auto'> 
     <Login/>
     {/* //formerly overflow-hidden */}
-    {/* <Signup/> */}
+    <Signup/>
     {/* <Sidebar/>
     <Chatbox/> */}
     
